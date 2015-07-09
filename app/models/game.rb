0fba83d74d0f259be503cc62f9e2831230a70e2f
Ted Xiao <t.xiao@berkeley.edu>
2015-07-09 02:30:12 -0700

    Styling for new games view
    TODO: add user to new game automatically

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ActiveRecord::Base
-  belongs_to :user
+    belongs_to :user
+
+    after_initialize :defaults
+
+    def defaults
+        self.score ||= 0
+        self.progress ||= 0
+        self.completed ||= false
+    end
 end
