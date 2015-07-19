ca4782b90c4cbbfa7179008dcc78808b24ad3356
Ted Xiao <t.xiao@berkeley.edu>
2015-07-18 23:42:05 -0700

    Added non-persisting last guess delta to stats box
    TODO: Add last guess delta to games model

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -94,8 +94,8 @@ class GamesController < ApplicationController
 
   def make_guess
     @question = @game.questions[@game.progress]
-    error = (((params[:lat].to_f - @question.lat)*10000*3280.4/90)**2 + ((params[:long].to_f - @question.long)*10000*3280.4/90)**2)**0.5
-    newscore = @game.score + 5000 - error
+    @delta = (((params[:lat].to_f - @question.lat)*10000*3280.4/90)**2 + ((params[:long].to_f - @question.long)*10000*3280.4/90)**2)**0.5
+    newscore = @game.score + 5000 - @delta
     @game.update_attribute(:score, newscore)
     if @game.progress.equal?(@game.questions.length - 1)
       @game.update_attribute(:progress, 0)
@@ -103,8 +103,10 @@ class GamesController < ApplicationController
       @game.update_attribute(:progress, @game.progress + 1)
     end
     @question = @game.questions[@game.progress]
+    @delta = (@delta*100000).round / 100000.0
+      
     respond_to do |format|
-      format.js { render "make_guess", :locals => {:error => error}}
+      format.js { render "make_guess", :locals => {:delta => @delta}}
     end
   end
 
