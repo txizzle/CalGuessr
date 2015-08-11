370b4e98567b7e553eef0101d9d60a64d37776d1
Ted Xiao <t.xiao@berkeley.edu>
2015-08-11 02:54:08 -0700

    Added account stuff and admin instead of view games/questions

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,10 +1,11 @@
 class Admin::BaseController < ApplicationController
+  before_action :require_admin!
 
 
   def index
     @last_signups = User.last_signups(10)
     @last_signins = User.last_signins(10)
     @count = User.users_count
-    
+
   end
 end
